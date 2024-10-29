d7a4dbb8a8aa9a863829f1a004c270d035ab65c8
tgr <thaiguer.k@gmail.com>
2024-10-29 07:13:23 -0300

    initial test

---
+++ Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Urls.Add("http://localhost:5029");
+app.Urls.Add("http://192.168.1.6:5029");
+
+app.MapGet("/", () => GetHandle());
+
+app.Run();
+
+string GetHandle()
+{
+    DateTime a = DateTime.Now;
+    return a.ToString();
+}
